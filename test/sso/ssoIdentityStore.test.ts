3717fa957e120e118e5e9882e54a6b0b5cdedae9
Mat Werber <matwerber@gmail.com>
2024-11-26 08:35:59 -0800

    add SsoIdentityStore construct

---
+++ test/sso/ssoIdentityStore.test.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { SsoIdentityStore } from '../../src/sso/ssoIdentityStore';
+
+test('SsoIdentityStore creates expected resources', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'TestStack');
+  new SsoIdentityStore(stack, 'TestIdentityStore', {});
+  const template = Template.fromStack(stack);
+  console.debug('Synthesized Template:', JSON.stringify(template.toJSON(), null, 2));
+  const createUpdateMatchValue = Match.objectEquals({
+    'Fn::Join': Match.arrayEquals(
+      ['', Match.arrayEquals([
+        '{\"service\":\"SSOAdmin\",\"action\":\"listInstances\",\"parameters\":{\"region\":\"',
+        Match.objectEquals({
+          Ref: 'AWS::Region',
+        }),
+        Match.anyValue(),
+      ])],
+    ),
+  });
+  template.hasResourceProperties('Custom::SSO-IdentityStore', {
+    Create: createUpdateMatchValue,
+    Update: createUpdateMatchValue,
+  });
+});
